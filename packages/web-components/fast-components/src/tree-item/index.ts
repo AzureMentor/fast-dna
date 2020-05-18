microsoft/fast-react

add tree item outline

packages/web-components/fast-components/src/tree-item/index.ts
@@ -1,0 +1,14 @@
+import { customElement } from "@microsoft/fast-element";
+import { TreeItem } from "./tree-item";
+import { TreeItemTemplate as template } from "./tree-item.template";
+import { TreeItemStyles as styles } from "./tree-item.styles";
+
+@customElement({
+    name: "fast-tree-item",
+    template,
+    styles,
+})
+export class FASTCard extends TreeItem {}
+export * from "./tree-item.template";
+export * from "./tree-item.styles";
+export * from "./tree-item";

AzureMentor/fast-dna

packages/web-components/fast-components/src/tree-item/index.ts
@@ -1,0 +1,14 @@
+import { customElement } from "@microsoft/fast-element";
+import { TreeItem } from "./tree-item";
+import { TreeItemTemplate as template } from "./tree-item.template";
+import { TreeItemStyles as styles } from "./tree-item.styles";
+
+@customElement({
+    name: "fast-tree-item",
+    template,
+    styles,
+})
+export class FASTTreeItem extends TreeItem {}
+export * from "./tree-item.template";
+export * from "./tree-item.styles";
+export * from "./tree-item";